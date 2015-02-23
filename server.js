2f92f1e1bb9dca2c390c480b942850c0e2911e37
Armin Akhavan <armin.akhavan@gmail.com>
2015-02-23 10:27:04 -0500

    limit500

--- server.js
+++ server.js
@@ -141,7 +141,7 @@
   ArtistSchema.methods.findLimited = function(cb) {
     var query;
     query = this.model('Artist').find({});
-    query.limit(1000);
+    query.limit(500);
     return query.exec(cb);
   };
 
